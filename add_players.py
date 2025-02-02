2b217ade0b44df02f864703307425b6530aab620
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2025-02-03 04:02:18 +0700

    feat: can't add player with the same name

--- add_players.py
+++ add_players.py
@@ -1,8 +1,9 @@
 from singleton import connect_database as connector
 from singleton import players
 
-# we save all the row data to be inserted in a data variable
-players_data = []
+new_players = []
+existing_players = players.get_all_data
+existing_players = list(zip(*existing_players))[1]
 
 print("\n--- Add New Players ---")
 print("Please enter player names one by one.")
@@ -16,19 +17,19 @@ while True:
         print("\n✅ Finished adding players.")
         break
 
-    # if player_name in database["players"]:
-    #     print(f"\n⚠️ Player '{player_name}' already exists! Please choose a different name.\n")
-    if player_name == "":
+    if player_name in existing_players:
+        print(f"\n⚠️ Player '{player_name}' already exists! Please choose a different name.\n")
+    elif player_name == "":
         print("\n⚠️ Player name cannot be empty. Please try again.\n")
     else:
-        players_data.append(player_name)
+        new_players.append(player_name)
 
-# convert list to list of tuples
-players_data = zip(*[iter(players_data)]*1)
+        # convert list to list of tuples
+        new_players = zip(*[iter(new_players)]*1)
 
-# execute the insert commands for all rows and commit to the database
-connector.c.executemany(players.query_insert, players_data)
-connector.db.commit()
+        # execute the insert commands for all rows and commit to the database
+        connector.c.executemany(players.query_insert, new_players)
+        connector.db.commit()
 
 # finally closing the database connection
 connector.db.close()
