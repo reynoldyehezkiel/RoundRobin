4ef7c64c68b6fe01f120a9f92294b8fc3bd26e74
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2025-02-04 18:16:49 +0700

    Update add_players.py

--- add_players.py
+++ add_players.py
@@ -10,12 +10,13 @@ print("Type 'done' when you are finished adding players.\n")
 
 while True:
     # Asking for player name with instructions
-    player_name = input("Enter a player name: ").strip()
+    input_player = input("Enter a player name: ").strip()
 
-    if player_name.lower() == "done":
-        print("\n✅ Finished adding players.")
+    if input_player.lower() == "done":
+        print("\n✅ Player saved successfully.")
         break
 
+<<<<<<< Updated upstream
     # if player_name in database["players"]:
     #     print(f"\n⚠️ Player '{player_name}' already exists! Please choose a different name.\n")
     if player_name == "":
@@ -29,6 +30,22 @@ players_data = zip(*[iter(players_data)]*1)
 # execute the insert commands for all rows and commit to the database
 connector.c.executemany(players.query_insert, players_data)
 connector.db.commit()
+=======
+    if input_player in existing_players:
+        print(f"\n⚠️ Player '{input_player}' already exists! Please choose a different name.\n")
+    elif input_player == "":
+        print("\n⚠️ Player name cannot be empty. Please try again.\n")
+    else:
+        new_players.append(input_player)
+        print(new_players)
+
+        # convert list to list of tuples
+        # new_players = zip(*[iter(new_players)]*1)
+
+        # execute the insert commands for all rows and commit to the database
+        # connector.c.executemany(players.query_insert, new_players)
+        connector.db.commit()
+>>>>>>> Stashed changes
 
 # finally closing the database connection
 connector.db.close()
