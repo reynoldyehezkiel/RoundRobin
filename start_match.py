870f5abc168eea701184a9388fa83199ab410d17
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2025-02-05 16:31:02 +0700

    Feat: match all players & match by certain player

--- start_match.py
+++ start_match.py
@@ -2,6 +2,30 @@ from singleton import connect_database as connector
 from singleton import players
 from singleton import matches
 
+def match_players(match_data, p1_id, p1_name, p2_id, p2_name):
+        print(f"\nMatch: {player1_name} vs {player2_name}")
+        print("Please enter '1' for player 1 or '2' for player 2.")
+        print("Leave blank and press Enter to skip the match.\n")
+
+        while True:
+            winner_choice = input(f"Enter your choice (1 for {player1_name} / 2 for {player2_name}): ").strip()
+            if winner_choice == "1":
+                connector.c.execute(matches.set_winner, (player1_id, match_id))
+                print(f"\n✅ Winner: {player1_name}!")
+                break
+
+            elif winner_choice == "2":
+                connector.c.execute(matches.set_winner, (player2_id, match_id))
+                print(f"\n✅ Winner: {player2_name}!")
+                break
+
+            elif winner_choice == "":
+                print(f"\n⚠️ Match between {player1_name} and {player2_name} skipped.")
+                break
+
+            else:
+                print("\n❌ Invalid choice! Please enter '1' or '2' to record a winner or leave blank to skip.")
+
 # remaining match data if winner_id null
 remain_matches = matches.get_remain_data
 
@@ -9,51 +33,45 @@ remain_matches = matches.get_remain_data
 players_data = players.get_all_data
 data_players_id = matches.update_total_win(players_data)
 
-# print all players data
-players.print_all_data()
-id_input = int(input("Choose player to delete by ID: "))
-
-# print(remain_matches)
-# for i in range(0, len(remain_matches)):
-#     if remain_matches[i][3] == id_input or remain_matches[i][1] == id_input:
-
 if not remain_matches:
     print("\nNo matches available. Generate new matches first.")
 else:
-    for match in remain_matches:
-        match_id = match[0]
-        player1_id = match[1]
-        player2_id = match[3]
-        player1_name = match[2]
-        player2_name = match[4]
+    # show menu
+    print("1. All matches")
+    print("2. Matches by player")
+    menu_input = int(input("Choose Menu: "))
 
-        print(f"\nMatch: {player1_name} vs {player2_name}")
-        print("Please enter '1' for player 1 or '2' for player 2.")
-        print("Leave blank and press Enter to skip the match.\n")
+    if menu_input == 1:
+        for match in remain_matches:
+            match_id = match[0]
+            player1_id = match[1]
+            player2_id = match[3]
+            player1_name = match[2]
+            player2_name = match[4]
 
-        if match[3] == id_input or match[1] == id_input:
-            while True:
-                winner_choice = input(f"Enter your choice (1 for {player1_name} / 2 for {player2_name}): ").strip()
-                if winner_choice == "1":
-                    connector.c.execute(matches.set_winner, (player1_id, match_id))
-                    print(f"\n✅ Winner: {player1_name}!")
-                    break
+            match_players(remain_matches, player1_id, player1_name, player2_id, player2_name)
 
-                elif winner_choice == "2":
-                    connector.c.execute(matches.set_winner, (player2_id, match_id))
-                    print(f"\n✅ Winner: {player2_name}!")
-                    break
+    elif menu_input == 2:
+        # print all players data
+        players.print_all_data()
+        id_input = int(input("\nChoose player to start match: "))
 
-                elif winner_choice == "":
-                    print(f"\n⚠️ Match between {player1_name} and {player2_name} skipped.")
-                    break
+        for match in remain_matches:
+            match_id = match[0]
+            player1_id = match[1]
+            player2_id = match[3]
+            player1_name = match[2]
+            player2_name = match[4]
 
-                else:
-                    print("\n❌ Invalid choice! Please enter '1' or '2' to record a winner or leave blank to skip.")
+            if match[3] == id_input or match[1] == id_input:
+                match_players(remain_matches, player1_id, player1_name, player2_id, player2_name)
+       
+    else:
+        print("\n❌ Invalid choice!")
 
-# connector.c.executemany(matches.set_total_win, data_players_id)
-# connector.db.commit()
-print(f"\n✅ All matches recorded")
+    connector.c.executemany(matches.set_total_win, data_players_id)
+    connector.db.commit()
+    print(f"\n✅ All matches recorded")
 
 # finally closing the database connection
 connector.db.close()
