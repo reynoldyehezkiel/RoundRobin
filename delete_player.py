b936b59f93ddd06ff62467c3474c4ef857080b1a
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2025-02-07 18:21:14 +0700

    Feature: cancel delete

--- delete_player.py
+++ delete_player.py
@@ -5,29 +5,37 @@ from singleton import matches
 # Get all players name
 players_data = players.get_all_data
 
-existing_players = []
+# Get id player
+player_id = list(zip(*players_data))[0]
 
-if players_data:
-    existing_players = list(zip(*players_data))[0]
+############## Add feature cancel delete ##############
 
 while True:
-    # print all players data
-    players.print_data(players_data)
-    id_input = int(input("\nChoose player by ID to delete: "))
 
-    if id_input not in existing_players:
+    print("Type player ID to delete")
+    print("⚠️ Type 0 to cancel\n")
+
+    # Print all players data
+    players.print_data(players_data)
+    id_input = int(input("\nChoose player: "))
+
+    if id_input == 0:
+        print(f"\nDelete player is cancel. No players are deleted.")
+        break
+    elif id_input not in player_id:
         print(f"\n❌ Player is not in the list. Make sure to input the right ID!\n")
     else:
+        # Get player name
         player_name = ""
         for i in range(0,len(players_data)):
             if id_input == players_data[i][0]:
                 player_name = players_data[i][1]
 
-        # get list of player id to update total_win
-        data_players_id = matches.update_total_win(players_data)
+        # update total win
+        data_total_win = matches.update_total_win(players_data)
 
         # connector.c.execute(players.query_delete, (id_input,))
-        # connector.c.executemany(matches.set_total_win, data_players_id)
+        # connector.c.executemany(matches.set_total_win, data_total_win)
         connector.db.commit()
         print(f"\n✅ Player '{player_name}' deleted successfully!")
 
